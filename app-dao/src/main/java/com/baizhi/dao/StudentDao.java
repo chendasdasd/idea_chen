88d5315521e0f1875e4a63aa361ec498e532afab
chen <chen@zparkhr.com.cn>
2018-07-02 19:09:10 +0800

    初始app-dao代码

---
+++ app-dao/src/main/java/com/baizhi/dao/StudentDao.java
@@ -0,0 +1,21 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.Student;
+
+import java.util.List;
+
+/**
+ * Created by 陈少 on 2018/7/1.
+ */
+public interface StudentDao {
+
+    public List<Student> selectAll();
+
+    public Student selectById(Integer id);
+
+    public void update(Student student);
+
+    public void insert(Student student);
+
+    public void delete(Integer id);
+}
